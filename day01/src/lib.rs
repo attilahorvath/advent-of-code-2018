f218a6c0e0f0caf1898a7db63471977cc4f3a261
Attila Horváth <hun.ati500@gmail.com>
2018-12-04 00:41:30 +0000

    Add solution for day 1 puzzle

---
+++ day01/src/lib.rs
@@ -0,0 +1,72 @@
+use std::collections::HashSet;
+
+pub fn resulting_frequency(changes: &[i32]) -> i32 {
+    changes.iter().sum()
+}
+
+pub fn duplicate_frequency(changes: &[i32]) -> i32 {
+    let mut frequencies = HashSet::new();
+    let mut frequency = 0;
+
+    frequencies.insert(frequency);
+
+    for change in changes.iter().cycle() {
+        frequency += change;
+
+        if !frequencies.insert(frequency) {
+            return frequency;
+        }
+    }
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_resulting_frequency() {
+        assert_eq!(3, resulting_frequency(&[1, -2, 3, 1]));
+    }
+
+    #[test]
+    fn resulting_frequency_of_positive_changes() {
+        assert_eq!(3, resulting_frequency(&[1, 1, 1]));
+    }
+
+    #[test]
+    fn resulting_frequency_of_mixed_changes() {
+        assert_eq!(0, resulting_frequency(&[1, 1, -2]));
+    }
+
+    #[test]
+    fn resulting_frequency_of_negative_changes() {
+        assert_eq!(-6, resulting_frequency(&[-1, -2, -3]));
+    }
+
+    #[test]
+    fn sample_duplicate_frequency() {
+        assert_eq!(2, duplicate_frequency(&[1, -2, 3, 1]));
+    }
+
+    #[test]
+    fn zero_duplicate_frequency() {
+        assert_eq!(0, duplicate_frequency(&[1, -1]));
+    }
+
+    #[test]
+    fn ten_duplicate_frequency() {
+        assert_eq!(10, duplicate_frequency(&[3, 3, 4, -2, -4]));
+    }
+
+    #[test]
+    fn five_duplicate_frequency() {
+        assert_eq!(5, duplicate_frequency(&[-6, 3, 8, 5, -6]));
+    }
+
+    #[test]
+    fn fourteen_duplicate_frequency() {
+        assert_eq!(14, duplicate_frequency(&[7, 7, -2, -7, -4]));
+    }
+}
